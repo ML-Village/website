fbc85bd77f14cc99a0c1b0f18fd505ae1cad61f5
0xGavinOwen <gavinowen713@gmail.com>
2024-04-18 01:00:46 +0800

    feat: add framer motion

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import naruto from "./assets/blurr-crop.png";
 import Navbar from "./components/Navbar";
 
@@ -7,22 +8,41 @@ function App() {
       <Navbar></Navbar>
       <div className="flex justify-center gap-15 py-14">
         <div>
-          <h1 className="text-7xl font-bold text-[#1E4387] mb-4 font-NjNaruto mt-4">
-            ML Village
-          </h1>
-          <h2 className="text-5xl font-semibold text-[#fff] mb-2 py-10 leading-tight">
-            A MarketPlace For Open <br /> Source AI Models & Agents <br /> on{" "}
-            <span className="text-[#0C0C4F]">Starknet</span>
-          </h2>
-          <p className="text-xl text-gray-600">
-            - Ushering in the age of AI and Gaming -
-          </p>
-          <button
-            className=" text-xl mt-14 h-16 w-96 border-2 rounded-3xl bg-[#BCBFD2] text-[#0C0C4F] opacity-50"
-            disabled
+          <motion.div
+            initial={{ y: 150 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.8 }}
           >
-            Notion Page Coming Soon ...
-          </button>
+            <h1 className="text-7xl font-bold text-[#1E4387] mb-4 font-NjNaruto mt-4">
+              ML Village
+            </h1>
+            <h2 className="text-5xl font-semibold text-[#fff] mb-2 py-10 leading-tight">
+              A MarketPlace For Open <br /> Source AI Models & Agents <br /> on{" "}
+              <span className="text-[#0C0C4F]">Starknet</span>
+            </h2>
+          </motion.div>
+
+          <motion.div
+            initial={{ y: 150 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.9 }}
+          >
+            <p className="text-xl text-gray-600">
+              - Ushering in the age of AI and Gaming -
+            </p>
+          </motion.div>
+          <motion.div
+            initial={{ y: 150 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <button
+              className="text-xl mt-14 h-16 w-96 border-2 rounded-3xl bg-[#BCBFD2] text-[#0C0C4F] opacity-50"
+              disabled
+            >
+              Notion Page Coming Soon ...
+            </button>
+          </motion.div>
         </div>
         <img src={naruto} alt="naruto" className="h-[555px] mr-10" />
       </div>
